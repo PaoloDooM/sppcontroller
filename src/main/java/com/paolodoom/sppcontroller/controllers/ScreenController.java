57f87e4480d16a454583bea6ca7a92ca2860cfab
PAOLO BUFALINO MORA <21-e7110311@users.noreply.gitlab.tjrr.jus.br>
2022-04-28 23:30:25 -0300

    adds treeView in screenCtrl (needs to run in a isolated thread)

--- src/main/java/com/paolodoom/sppcontroller/controllers/ScreenController.java
+++ src/main/java/com/paolodoom/sppcontroller/controllers/ScreenController.java
@@ -5,20 +5,26 @@
  */
 package com.paolodoom.sppcontroller.controllers;
 
+import com.paolodoom.sppcontroller.models.CustomTreeMap;
 import com.profesorfalken.jsensors.JSensors;
 import com.profesorfalken.jsensors.model.components.Components;
 import com.profesorfalken.jsensors.model.components.Cpu;
 import com.profesorfalken.jsensors.model.components.Disk;
 import com.profesorfalken.jsensors.model.components.Gpu;
+import com.profesorfalken.jsensors.model.components.Mobo;
 import com.profesorfalken.jsensors.model.sensors.Fan;
 import com.profesorfalken.jsensors.model.sensors.Load;
 import com.profesorfalken.jsensors.model.sensors.Temperature;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.text.Text;
 
 /**
@@ -33,6 +39,8 @@ public class ScreenController implements Initializable {
 
     @FXML
     private Text sensorsDisplay;
+    @FXML
+    private TreeView<Map> treeView;
 
     /**
      * Initializes the controller class.
@@ -43,7 +51,7 @@ public class ScreenController implements Initializable {
         new Thread(sensorsTask).start();
     }
 
-    public void periodicRead() {
+    public Components periodicRead() {
         Components components = JSensors.get.components();
         sensorsDisplay.setText("");
         List<Cpu> cpus = components.cpus;
@@ -108,42 +116,72 @@ public class ScreenController implements Initializable {
                     }
                 }
             }
+        }
 
-            List<Disk> disks = components.disks;
-            if (disks != null) {
-                int i = 0;
-                for (final Disk disk : disks) {
-                    System.out.println("Found Disk component: " + disk.name);
-                    sensorsDisplay.setText(sensorsDisplay.getText() + disk.name + " " + i +"\n");
+        List<Disk> disks = components.disks;
+        if (disks != null) {
+            for (int i = 0; i < disks.size(); i++) {
+                System.out.println("Found Disk component: " + disks.get(i).name);
+                sensorsDisplay.setText(sensorsDisplay.getText() + disks.get(i).name + " " + i + "\n");
 
-                    if (disk.sensors != null) {
-                        System.out.println("Sensors: ");
+                if (disks.get(i).sensors != null) {
+                    System.out.println("Sensors: ");
 
-                        //Print temperatures
-                        List<Temperature> temps = disk.sensors.temperatures;
-                        for (final Temperature temp : temps) {
-                            System.out.println(temp.name + ": " + temp.value + " C");
-                            sensorsDisplay.setText(sensorsDisplay.getText() + "   " + temp.name + ": " + temp.value + " C" + "\n");
-                        }
+                    //Print temperatures
+                    List<Temperature> temps = disks.get(i).sensors.temperatures;
+                    for (final Temperature temp : temps) {
+                        System.out.println(temp.name + ": " + temp.value + " C");
+                        sensorsDisplay.setText(sensorsDisplay.getText() + "   " + temp.name + ": " + temp.value + " C" + "\n");
+                    }
 
-                        //Print fan speed
-                        List<Fan> fans = disk.sensors.fans;
-                        for (final Fan fan : fans) {
-                            System.out.println(fan.name + ": " + fan.value + " RPM");
-                            sensorsDisplay.setText(sensorsDisplay.getText() + "   " + fan.name + ": " + fan.value + " RPM" + "\n");
-                        }
+                    //Print fan speed
+                    List<Fan> fans = disks.get(i).sensors.fans;
+                    for (final Fan fan : fans) {
+                        System.out.println(fan.name + ": " + fan.value + " RPM");
+                        sensorsDisplay.setText(sensorsDisplay.getText() + "   " + fan.name + ": " + fan.value + " RPM" + "\n");
+                    }
 
-                        List<Load> loads = disk.sensors.loads;
-                        for (final Load load : loads) {
-                            System.out.println(load.name + ": " + load.value + " %");
-                            sensorsDisplay.setText(sensorsDisplay.getText() + "   " + load.name + ": " + load.value + " %" + "\n");
-                        }
-                        i++;
+                    List<Load> loads = disks.get(i).sensors.loads;
+                    for (final Load load : loads) {
+                        System.out.println(load.name + ": " + load.value + " %");
+                        sensorsDisplay.setText(sensorsDisplay.getText() + "   " + load.name + ": " + load.value + " %" + "\n");
+                    }
+                }
+            }
+        }
+        List<Mobo> mobos = components.mobos;
+        if (mobos != null) {
+            for (final Mobo mobo : mobos) {
+                System.out.println("Found Mobo component: " + mobo.name);
+                sensorsDisplay.setText(sensorsDisplay.getText() + mobo.name + "\n");
+
+                if (mobo.sensors != null) {
+                    System.out.println("Sensors: ");
+
+                    //Print temperatures
+                    List<Temperature> temps = mobo.sensors.temperatures;
+                    for (final Temperature temp : temps) {
+                        System.out.println(temp.name + ": " + temp.value + " C");
+                        sensorsDisplay.setText(sensorsDisplay.getText() + "   " + temp.name + ": " + temp.value + " C" + "\n");
+                    }
+
+                    //Print fan speed
+                    List<Fan> fans = mobo.sensors.fans;
+                    for (final Fan fan : fans) {
+                        System.out.println(fan.name + ": " + fan.value + " RPM");
+                        sensorsDisplay.setText(sensorsDisplay.getText() + "   " + fan.name + ": " + fan.value + " RPM" + "\n");
+                    }
+
+                    List<Load> loads = mobo.sensors.loads;
+                    for (final Load load : loads) {
+                        System.out.println(load.name + ": " + load.value + " %");
+                        sensorsDisplay.setText(sensorsDisplay.getText() + "   " + load.name + ": " + load.value + " %" + "\n");
                     }
                 }
             }
         }
         System.out.println("------------------------------------------------------------");
+        return components;
     }
 
     public void createSensorsTask() {
@@ -151,11 +189,16 @@ public class ScreenController implements Initializable {
             sensorsTask = new Task<Void>() {
                 @Override
                 public Void call() {
+                    boolean firstIteration = true;
                     while (!isCancelled()) {
                         try {
-                            periodicRead();
+                            Components components = periodicRead();
+                            if (firstIteration) {
+                                initializeviewTree(components);
+                                firstIteration = false;
+                            }
                             //TODO: maybe timer
-                            Thread. sleep(200);
+                            Thread.sleep(1000);
                         } catch (Exception ex) {
                             sensorsDisplay.setText("Error:\n" + ex.toString());
                         }
@@ -184,5 +227,128 @@ public class ScreenController implements Initializable {
             sensorsTask = null;
         }
         sensorsDisplay.setText("Stopped");
+    }
+
+    public void initializeviewTree(Components components) {
+        List<Cpu> cpus = components.cpus;
+        List<Gpu> gpus = components.gpus;
+        List<Disk> disks = components.disks;
+        List<Mobo> mobos = components.mobos;
+
+        TreeItem<Map> rootTree = new TreeItem<>(new CustomTreeMap("Sensors", Arrays.asList(0), false));
+
+        if (cpus != null) {
+            TreeItem<Map> deviceTree = new TreeItem<>(new CustomTreeMap("Cpus", Arrays.asList(0, 0), false));
+            for (int i = 0; i < cpus.size(); i++) {
+                TreeItem<Map> cpuTree = new TreeItem<>(new CustomTreeMap(cpus.get(i).name, Arrays.asList(0, 0, i), false));
+                if (cpus.get(i).sensors != null) {
+                    TreeItem<Map> loadTree = new TreeItem<>(new CustomTreeMap("Load", Arrays.asList(0, 0, i, 0), false));
+                    for (int j = 0; j < cpus.get(i).sensors.loads.size(); j++) {
+                        loadTree.getChildren().add(new TreeItem<>(new CustomTreeMap(cpus.get(i).sensors.loads.get(j).name, Arrays.asList(0, 0, i, 0, j), true)));
+                    }
+                    cpuTree.getChildren().add(loadTree);
+                    TreeItem<Map> tempTree = new TreeItem<>(new CustomTreeMap("Temp", Arrays.asList(0, 0, i, 1), false));
+                    for (int j = 0; j < cpus.get(i).sensors.temperatures.size(); j++) {
+                        tempTree.getChildren().add(new TreeItem<>(new CustomTreeMap(cpus.get(i).sensors.temperatures.get(j).name, Arrays.asList(0, 0, i, 1, j), true)));
+                    }
+                    cpuTree.getChildren().add(tempTree);
+                    TreeItem<Map> fanTree = new TreeItem<>(new CustomTreeMap("Fan", Arrays.asList(0, 0, i, 2), false));
+                    for (int j = 0; j < cpus.get(i).sensors.fans.size(); j++) {
+                        fanTree.getChildren().add(new TreeItem<>(new CustomTreeMap(cpus.get(i).sensors.fans.get(j).name, Arrays.asList(0, 0, i, 2, j), true)));
+                    }
+                    cpuTree.getChildren().add(fanTree);
+                }
+                deviceTree.getChildren().add(cpuTree);
+            }
+            rootTree.getChildren().add(deviceTree);
+        }
+        if (gpus != null) {
+            TreeItem<Map> deviceTree = new TreeItem<>(new CustomTreeMap("Gpus", Arrays.asList(0, 1), false));
+            for (int i = 0; i < gpus.size(); i++) {
+                TreeItem<Map> gpuTree = new TreeItem<>(new CustomTreeMap(gpus.get(i).name, Arrays.asList(0, 1, i), false));
+                if (gpus.get(i).sensors != null) {
+                    TreeItem<Map> loadTree = new TreeItem<>(new CustomTreeMap("Load", Arrays.asList(0, 1, i, 0), false));
+                    for (int j = 0; j < gpus.get(i).sensors.loads.size(); j++) {
+                        loadTree.getChildren().add(new TreeItem<>(new CustomTreeMap(gpus.get(i).sensors.loads.get(j).name, Arrays.asList(0, 1, i, 0, j), true)));
+                    }
+                    gpuTree.getChildren().add(loadTree);
+                    TreeItem<Map> tempTree = new TreeItem<>(new CustomTreeMap("Temp", Arrays.asList(0, 1, i, 1), false));
+                    for (int j = 0; j < gpus.get(i).sensors.temperatures.size(); j++) {
+                        tempTree.getChildren().add(new TreeItem<>(new CustomTreeMap(gpus.get(i).sensors.temperatures.get(j).name, Arrays.asList(0, 1, i, 1, j), true)));
+                    }
+                    gpuTree.getChildren().add(tempTree);
+                    TreeItem<Map> fanTree = new TreeItem<>(new CustomTreeMap("Fan", Arrays.asList(0, 1, i, 2), false));
+                    for (int j = 0; j < gpus.get(i).sensors.fans.size(); j++) {
+                        fanTree.getChildren().add(new TreeItem<>(new CustomTreeMap(gpus.get(i).sensors.fans.get(j).name, Arrays.asList(0, 1, i, 2, j), true)));
+                    }
+                    gpuTree.getChildren().add(fanTree);
+                }
+                deviceTree.getChildren().add(gpuTree);
+            }
+            rootTree.getChildren().add(deviceTree);
+        }
+        if (disks != null) {
+            TreeItem<Map> deviceTree = new TreeItem<>(new CustomTreeMap("Disks", Arrays.asList(0, 2), false));
+            for (int i = 0; i < disks.size(); i++) {
+                TreeItem<Map> diskTree = new TreeItem<>(new CustomTreeMap(disks.get(i).name + " " + i, Arrays.asList(0, 2, i), false));
+                if (disks.get(i).sensors != null) {
+                    TreeItem<Map> loadTree = new TreeItem<>(new CustomTreeMap("Load", Arrays.asList(0, 2, i, 0), false));
+                    for (int j = 0; j < disks.get(i).sensors.loads.size(); j++) {
+                        loadTree.getChildren().add(new TreeItem<>(new CustomTreeMap(disks.get(i).sensors.loads.get(j).name, Arrays.asList(0, 2, i, 0, j), true)));
+                    }
+                    diskTree.getChildren().add(loadTree);
+                    TreeItem<Map> tempTree = new TreeItem<>(new CustomTreeMap("Temp", Arrays.asList(0, 2, i, 1), false));
+                    for (int j = 0; j < disks.get(i).sensors.temperatures.size(); j++) {
+                        tempTree.getChildren().add(new TreeItem<>(new CustomTreeMap(disks.get(i).sensors.temperatures.get(j).name, Arrays.asList(0, 2, i, 1, j), true)));
+                    }
+                    diskTree.getChildren().add(tempTree);
+                    TreeItem<Map> fanTree = new TreeItem<>(new CustomTreeMap("Fan", Arrays.asList(0, 2, i, 2), false));
+                    for (int j = 0; j < disks.get(i).sensors.fans.size(); j++) {
+                        fanTree.getChildren().add(new TreeItem<>(new CustomTreeMap(disks.get(i).sensors.fans.get(j).name, Arrays.asList(0, 2, i, 2, j), true)));
+                    }
+                    diskTree.getChildren().add(fanTree);
+                }
+                deviceTree.getChildren().add(diskTree);
+            }
+            rootTree.getChildren().add(deviceTree);
+        }
+        if (mobos != null) {
+            TreeItem<Map> deviceTree = new TreeItem<>(new CustomTreeMap("Mobo", Arrays.asList(0, 3), false));
+            for (int i = 0; i < mobos.size(); i++) {
+                TreeItem<Map> moboTree = new TreeItem<>(new CustomTreeMap(mobos.get(i).name == null ? "Generic Motherboard" : mobos.get(i).name, Arrays.asList(0, 3, i), false));
+                if (mobos.get(i).sensors != null) {
+                    TreeItem<Map> loadTree = new TreeItem<>(new CustomTreeMap("Load", Arrays.asList(0, 3, i, 0), false));
+                    for (int j = 0; j < mobos.get(i).sensors.loads.size(); j++) {
+                        loadTree.getChildren().add(new TreeItem<>(new CustomTreeMap(mobos.get(i).sensors.loads.get(j).name, Arrays.asList(0, 3, i, 0, j), true)));
+                    }
+                    moboTree.getChildren().add(loadTree);
+                    TreeItem<Map> tempTree = new TreeItem<>(new CustomTreeMap("Temp", Arrays.asList(0, 3, i, 1), false));
+                    for (int j = 0; j < mobos.get(i).sensors.temperatures.size(); j++) {
+                        tempTree.getChildren().add(new TreeItem<>(new CustomTreeMap(mobos.get(i).sensors.temperatures.get(j).name, Arrays.asList(0, 3, i, 1, j), true)));
+                    }
+                    moboTree.getChildren().add(tempTree);
+                    TreeItem<Map> fanTree = new TreeItem<>(new CustomTreeMap("Fan", Arrays.asList(0, 3, i, 2), false));
+                    for (int j = 0; j < mobos.get(i).sensors.fans.size(); j++) {
+                        fanTree.getChildren().add(new TreeItem<>(new CustomTreeMap(mobos.get(i).sensors.fans.get(j).name, Arrays.asList(0, 3, i, 2, j), true)));
+                    }
+                    moboTree.getChildren().add(fanTree);
+                }
+                deviceTree.getChildren().add(moboTree);
+            }
+            rootTree.getChildren().add(deviceTree);
+        }
+        //rootTree.setExpanded(true);
+        treeView.setRoot(rootTree);
+        //treeView.selectionModelProperty().get().selectFirst();
+        treeView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> updateSelectedItem(newValue));
+    }
+    
+    private void updateSelectedItem(TreeItem<Map> newValue) {
+        try {
+            System.out.println("toggled: " + newValue.getValue().get("position"));
+            System.out.println("interactive: " + newValue.getValue().get("interactive"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
